db1a129c1892357d9ac796254b820eb96b1ca777
Sheepinav <abhaskar@berkeley.edu>
2021-05-03 12:54:01 -0500

    fixing confusing variable names

--- myscript.js
+++ myscript.js
@@ -3,26 +3,26 @@ function setup() {
     background(153);
     noStroke();
     textSize(16)
-    slider = createSlider(0, 10, 5, 0.1)
-    slider.position((width - slider.width) / 2, (height / 1.5 - slider.height));
+    noiseSlider = createSlider(0, 10, 5, 0.1)
+    noiseSlider.position((width - noiseSlider.width) / 2, (height / 1.5 - noiseSlider.height));
     start = 0
     iSlider = createSlider(0, 0.1, 0.01, 0.01)
-    iSlider.position((width - slider.width) / 2, (height / 1.5 + slider.height));
+    iSlider.position((width - noiseSlider.width) / 2, (height / 1.5 + noiseSlider.height));
 }
 
 function draw() {
     background(153)
     textFont('Courier New')
-    sliderValue = slider.value()
-    text('Noise Detail:', slider.x - slider.width + 20, slider.y + slider.height -4)
-    text(sliderValue, slider.x + slider.width + 20, slider.y + slider.height-4 )
+    noiseSliderValue = noiseSlider.value()
+    text('Noise Detail:', noiseSlider.x - noiseSlider.width + 20, noiseSlider.y + noiseSlider.height - 4)
+    text(noiseSliderValue, noiseSlider.x + noiseSlider.width + 20, noiseSlider.y + noiseSlider.height - 4)
     increment = iSlider.value()
-    text('Increment:', iSlider.x - iSlider.width + 20, iSlider.y + iSlider.height -4)
-    text(increment, iSlider.x + iSlider.width + 20, iSlider.y + iSlider.height-4 )
+    text('Increment:', iSlider.x - iSlider.width + 20, iSlider.y + iSlider.height - 4)
+    text(increment, iSlider.x + iSlider.width + 20, iSlider.y + iSlider.height - 4)
     stroke(255)
     noFill()
     beginShape()
-    noiseDetail(sliderValue)
+    noiseDetail(noiseSliderValue)
     xoff = start
     for (x = 0; x < width; x++) {
         let y = noise(xoff) * height / 1.8 + 20
@@ -37,4 +37,4 @@ function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     slider.position((width - slider.width) / 2, (height / 1.5 - slider.height));
     iSlider.position((width - slider.width) / 2, (height / 1.5 + slider.height));
-  }
+}
